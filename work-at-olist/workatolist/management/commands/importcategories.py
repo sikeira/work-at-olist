919541848d98f28f4ae67bb8c27e240a4d0e2cea
Gustavo Siqueira <gus_rsiqueira@hotmail.com>
2017-03-16 22:17:06 -0300

    Initial commit, importcategories command

---
+++ work-at-olist/workatolist/management/commands/importcategories.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Import channels category from csv.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('arguments', nargs='*')
+
+    def handle(self, *args, **options):
+        if len(options['arguments']) == 2:
+            channel = options['arguments'][0]
+            file = options['arguments'][1]
+            self.stdout.write(self.style.SUCCESS('Successfully imported categories'))
+        else:
+            self.stdout.write(self.style.ERROR('Usage: importcategories [category] [file]'))
