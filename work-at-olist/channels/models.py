3a056bc5d3c9f30483cd51802428cadf1b503469
Gustavo Siqueira <gus_rsiqueira@hotmail.com>
2017-03-19 17:40:43 -0300

    added views and templates list and categories

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -3,9 +3,31 @@ import uuid
 from django.db import models
 
 
+class ChannelCategoryManager(models.Manager):
+    def get_subcategories(self, channel, category):
+        categories = []
+        sub_categories = ChannelCategory.objects.filter(channel=channel,
+                                                       parent_category=category)
+        for sub_category in sub_categories:
+            categories.append((sub_category, self.get_subcategories(channel,
+                                                                    sub_category)))
+
+        return categories
+
+    def get_categories_tree(self, channel):
+        categories = []
+        root_categories = ChannelCategory.objects.filter(channel=channel,
+                                                         parent_category=None)
+        for category in root_categories:
+            categories.append((category, self.get_subcategories(channel,
+                                                                category)))
+        return categories 
+
+
 class Channel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
+    objects = ChannelCategoryManager()
 
     def __str__(self):
         return self.name
--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from channels.models import Channel, ChannelCategory
+
 # Create your views here.
+class ChannelsView:
+    def list(request):
+        template = 'home.html'
+        channels = Channel.objects.all()
+        return render(request, template, {'channels': channels})
+
+
+    def categories(request):
+        template = 'categories.html'
+        channel_id = request.GET.get('channel_id', None)
+        channel = Channel.objects.get(id=channel_id)
+        categories = Channel.objects.get_categories_tree(channel)
+        return render(request, template, {'categories': categories})
